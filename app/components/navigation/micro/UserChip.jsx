6cedc331d115502ff07309fe527b7f7244e29686
HifiComputer <rniumo@gmail.com>
2024-06-02 17:55:12 +0600

    userchip skeleton

--- app/components/navigation/micro/UserChip.jsx
+++ app/components/navigation/micro/UserChip.jsx
@@ -1,10 +1,27 @@
 'use client'
 
-import React from 'react';
-import { Avatar, Typography, Stack } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Avatar, Typography, Stack, Skeleton } from '@mui/material';
 import Link from 'next/link';
 
 const UserChip = () => {
+    const [isMounted, setisMounted] = useState(false);
+
+    useEffect(() => {
+        setisMounted(true)
+    }, [])
+
+    if (!isMounted) {
+        return (
+            <Stack direction="row" alignItems="center" spacing={1}>
+                <Skeleton width="36px" height="36px" variant='circular' />
+                <Stack spacing={0.5}>
+                    <Skeleton width="100px" height="20px" variant='rounded' />
+                    <Skeleton width="70px" height="10px" variant='rounded' />
+                </Stack>
+            </Stack>
+        )
+    }
     return (
         <Stack direction="row" alignItems="center" spacing={1}>
             <Avatar
@@ -12,7 +29,7 @@ const UserChip = () => {
                 alt='Avatar'
             />
             <Link href="/login">
-                <Typography sx={{"&:hover": {color: 'primary'}}} >Login</Typography>
+                <Typography sx={{ "&:hover": { color: 'primary' } }} >User Login</Typography>
             </Link>
         </Stack>
     )
